f473e2bc204ed354780798e48e6b77126a769f0d
Andy <amillion3@gmail.com>
2018-05-26 09:03:07 -0500

    WIP

--- javascripts/weatherAPI.js
+++ javascripts/weatherAPI.js
@@ -8,10 +8,34 @@ const getZipcode = () => {
   zip = dataGK.getZipcode();
 };
 
+const processAPIResponse = () => {
+  makeAPIRequest()
+    .then(data => {
+    //do stuff here
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+const buildApiUrl = () => {
+  return `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=${key}`;
+};
+
 const makeAPIRequest = () => {
   zip = dataGK.getZipcode();
   key = apiKeys.apiKey();
+  const apiUrl = buildApiUrl();
   // make API call promise next
+  return new Promise((resolve, reject) => {
+    $.ajax(apiUrl)
+      .done(data => {
+        resolve(data);
+      }).
+      fail(err => {
+        reject(err);
+      });
+  });
 };
 
 module.exports = {
