b8f6b5eb47ff1153c60daa32d0bb938a8f8427a7
Andy <amillion3@gmail.com>
2018-06-05 21:06:50 -0500

    forms completed, for now. may need CSS tweaks later

---
+++ _firebase-stuff.js
@@ -0,0 +1,37 @@
+// Email and password Docs
+// https://firebase.google.com/docs/auth/web/password-auth
+//
+// Firebase docs onAuth Change:
+// https://firebase.google.com/docs/auth/web/manage-users
+
+// CREATE USER/PW COMBO
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  // ...
+});
+
+// SIGN IN
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  // ...
+});
+
+// AUTH STATE CHANGE
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+  } else {
+    // No user is signed in.
+  }
+});
+
+// SIGN OUT SUCCESS
+firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
